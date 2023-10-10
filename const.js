60b3164357dbe9deaa101b75626d4d687bc05b84
Marlon Quinde <71990962+Marlon-Quinde@users.noreply.github.com>
2023-10-09 23:29:30 -0500

    feat(tetris-javascript-vanilla): :sparkles: Proyecto Tetris JavaScript Vanilla

---
+++ const.js
@@ -0,0 +1,10 @@
+export const BLOCK_SIZE = 20;
+export const BOARD_WIDTH = 14;
+export const BOARD_HEIGTH = 30;
+
+export const EVEN_MOVEMENTS = {
+  LEFT: "ArrowLeft",
+  DOWN: "ArrowDown",
+  RIGHT: "ArrowRight",
+  UP: "ArrowUp",
+};
